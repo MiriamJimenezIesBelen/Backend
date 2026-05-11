254a8fd54047c865e68c18d2d56d5792703bef12
Miriam <mjimsan1710@g.educaand.es>
2026-05-11 13:14:24 +0200

    fix: quitar JsonProperty de Empresa

--- src/main/java/com/impactovisible/domain/Empresa.java
+++ src/main/java/com/impactovisible/domain/Empresa.java
@@ -1,16 +1,13 @@
 package com.impactovisible.domain;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.*;
 import java.util.List;
 
 @Entity
 @Table(name = "empresas")
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
+@Getter @Setter
+@AllArgsConstructor @NoArgsConstructor
 @Builder
 @ToString(exclude = "plantas")
 @EqualsAndHashCode(of = "idEmpresa")
@@ -18,36 +15,22 @@ public class Empresa {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
-  @Column(name = "id_empresa")
   private Long idEmpresa;
 
-  @Column(name = "numero_registro")
   private String numeroRegistro;
-
-  @Column(name = "nombre")
   private String nombre;
-
-  @Column(name = "password")
   private String password;
-
-  @Column(name = "sector")
   private String sector;
-
-  @Column(name = "pais")
   private String pais;
-
-  @Column(name = "ciudad")
   private String ciudad;
 
   @Enumerated(EnumType.STRING)
-  @Column(name = "tamano")
   private Tamano tamano;
 
-  @Column(name = "correo_contacto", unique = true, nullable = false)
+  @Column(unique = true, nullable = false)
   private String correoContacto;
 
   @Enumerated(EnumType.STRING)
-  @Column(name = "rol")
   private Rol rol;
 
   @OneToMany(mappedBy = "empresa")
