0b1629c458418447a05ec61571d1558e1dd6fc1a
Miriam <mjimsan1710@g.educaand.es>
2026-05-15 13:18:51 +0200

    fix query mediciones por empresa

--- src/main/java/com/impactovisible/repository/MedicionRepository.java
+++ src/main/java/com/impactovisible/repository/MedicionRepository.java
@@ -2,10 +2,12 @@ package com.impactovisible.repository;
 
 import com.impactovisible.domain.Medicion;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
 public interface MedicionRepository extends JpaRepository<Medicion, String> {
-  List<Medicion> findByEmpresa_IdEmpresa(Long idEmpresa);
-}
+  @Query("SELECT m FROM Medicion m WHERE m.planta.empresa.idEmpresa = :idEmpresa")
+  List<Medicion> findByEmpresa_IdEmpresa(@Param("idEmpresa") Long idEmpresa);}
